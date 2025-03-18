61398e29d9487a8256b64fca28deb5ab8b7b09b9
German Valdes <39346821+geresvb@users.noreply.github.com>
2025-03-17 23:38:30 -0500

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "TerraformRG"
-    storage_account_name  = "terraformbackend456"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
