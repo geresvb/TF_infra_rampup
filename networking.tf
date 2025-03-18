df366ec21793c1b80a9024217a64d078a9e9ab09
German Valdes <39346821+geresvb@users.noreply.github.com>
2025-03-18 01:15:37 -0500

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -6,31 +6,27 @@ resource "azurerm_virtual_network" "network_1" {
   resource_group_name = azurerm_resource_group.RG_1.name
 }
 
-
-#SUBNETS
+# SUBNETS
 resource "azurerm_subnet" "private_subnet" {
   name                 = "Private_subnet"
   resource_group_name  = azurerm_resource_group.RG_1.name
   virtual_network_name = azurerm_virtual_network.network_1.name
-  address_prefixes     = ["10.0.0.8/29"]
+  address_prefixes     = ["10.0.0.32/27"]
 }
 
 resource "azurerm_subnet" "public_subnet" {
   name                 = "Public_subnet"
   resource_group_name  = azurerm_resource_group.RG_1.name
   virtual_network_name = azurerm_virtual_network.network_1.name
-  address_prefixes     = ["10.0.0.16/29"]
+  address_prefixes     = ["10.0.0.64/27"]
 }
 
 resource "azurerm_subnet" "management_subnet" {
   name                 = "Management_subnet"
   resource_group_name  = azurerm_resource_group.RG_1.name
   virtual_network_name = azurerm_virtual_network.network_1.name
-  address_prefixes     = ["10.0.0.24/29"]
+  address_prefixes     = ["10.0.0.96/27"]
 }
-
-
-
 
 
 
