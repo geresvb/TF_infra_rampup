7a310169c45631ebbed9396839ab36835948834b
German Valdes <39346821+geresvb@users.noreply.github.com>
2025-03-17 23:31:46 -0500

    Delete resourcegroup.tf

--- resourcegroup.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "RG_1" {
-  name     = "Docker_Infrastructure1"
-  location = "East US"
-  tags = {
-    Tower   = "Wintel"
-    Owner   = "ValdesG"
-    project = "Terraform"
-  }
-}
