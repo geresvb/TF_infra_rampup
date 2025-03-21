e26afa27285b93713bbd8fd80843e9a28caa1d06
German Valdes <39346821+geresvb@users.noreply.github.com>
2025-03-21 10:29:58 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "RG_1" {
-  name     = "Terraform_Infrastructure2"
+  name     = "Terraform_Infrastructure"
   location = "East US"
   tags = {
     Tower   = "Wintel"
